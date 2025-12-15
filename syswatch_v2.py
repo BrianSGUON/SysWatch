e960cb61dc9a23187de6b0f5a69a56bf2c1fdcd1
BrianSGUON <165809873+BrianSGUON@users.noreply.github.com>
2025-12-15 17:22:21 +0100

    Syswatch2

--- syswatch_v2.py
+++ syswatch_v2.py
@@ -1,47 +1,14 @@
 from collector import collecter_tout
 
-def affichage(data_systeme):
-    print("=== SYSTÈME ===")
-    print(f"Système d'exploitation : {data_systeme['os']}")
-    print(f"Version               : {data_systeme['version']}")
-    print(f"Architecture           : {data_systeme['architecture']}")
-    print(f"Nom de la machine      : {data_systeme['hostname']}")
-    print("")
+donnees = collecter_tout()
 
-def afficher_cpu(data_cpu):
-    print("=== CPU ===")
-    print(f"Cœurs physiques : {data_cpu['coeurs_physiques']}")
-    print(f"Cœurs logiques  : {data_cpu['coeurs_logiques']}")
-    print(f"Utilisation CPU : {data_cpu['utilisation']} %")
-    print("")
-
-def afficher_memoire(data_memoire):
-    print("=== MÉMOIRE ===")
-    print(f"Mémoire totale      : {octets_vers_go(data_memoire['total'])}")
-    print(f"Mémoire disponible  : {octets_vers_go(data_memoire['disponible'])}")
-    print(f"Utilisation mémoire : {data_memoire['pourcentage']} %")
-    print("")
-
-def afficher_disques(data_disques):
-    print("=== DISQUES ===")
-    for disque in data_disques:
-        print(f"Point de montage : {disque['point_montage']}")
-        print(f"  Total     : {octets_vers_go(disque['total'])}")
-        print(f"  Utilisé   : {octets_vers_go(disque['utilise'])}")
-        print(f"  Utilisation : {disque['pourcentage']} %")
-        print("")
-
-def octets_vers_go(octets):
-    return f"{octets / (1024 ** 3):.2f} GB"
-
-def main():
+def afficher_tout():
     donnees = collecter_tout()
+    print("=== SysWatch v2.0 ===")
+    print(f"Timestamp : {donnees['timestamp']}\n")
+    print("Système :", donnees["systeme"])
+    print("CPU :", donnees["cpu"])
+    print("Mémoire :", donnees["memoire"])
+    print("Disques :", donnees["disques"])
 
-    print("=== SysWatch v2.0 ===\n")
-
-    affichage(donnees["systeme"])
-    afficher_cpu(donnees["cpu"])
-    afficher_memoire(donnees["memoire"])
-    afficher_disques(donnees["disques"])
-
-main()
+afficher_tout()
